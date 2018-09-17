8bb691a3391d516703e5ec06d54bbdfcba48ecb5
Schalk van Heerden <swvheerden@gmail.com>
2018-09-17 08:23:37 +0200

    removed white spaces and commits

--- src/ln/channel.rs
+++ src/ln/channel.rs
@@ -266,10 +266,7 @@ const INITIAL_COMMITMENT_NUMBER: u64 = (1 << 48) - 1;
 // calling channel_id() before we're set up or things like get_outbound_funding_signed on an
 // inbound channel.
 pub struct Channel {
-	///TODO: split limits and runtime setting here
-	/// channel limits cant change during channel existence
-	/// but other settings can, but they need an update broadcast before they can change. 
-	config : UserConfigurations, 
+	config : UserConfigurations,
 	user_id: u64,
 
 	channel_id: [u8; 32],
@@ -543,7 +540,7 @@ impl Channel {
 		if msg.max_accepted_htlcs > 483 {
 			return_error_message!("max_accpted_htlcs > 483");
 		}
-		//optional parameter checking 
+		//optional parameter checking
 		// MAY fail the channel if
 		if msg.funding_satoshis < configurations.channel_limits.funding_satoshis {
 			return_error_message!("funding satoshis is less than the user specified limit");
@@ -613,7 +610,7 @@ impl Channel {
 
 		let mut chan = Channel {
 			user_id: user_id,
-			config: (*configurations).clone(), 
+			config: (*configurations).clone(),
 			channel_id: msg.temporary_channel_id,
 			channel_state: (ChannelState::OurInitSent as u32) | (ChannelState::TheirInitSent as u32),
 			channel_outbound: false,
